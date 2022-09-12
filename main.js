2fc0d4f0616f3f784afcaeca9ae87e246426026f
BunnyTheGreat <81476131+BunnyTheGreat@users.noreply.github.com>
2022-09-11 20:57:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+leftwristX = 0;
+
+rightwristX = 0;
+
+difference=0;
+
 function setup() {
     canvas = createCanvas(400,400);
     canvas.position(560,150);
@@ -13,10 +19,20 @@ function modelLoaded() {
 
 function draw() {
     background('#34ebd8');
+    textSize(difference);
+    document.getElementById("font-size").innerHTML = "The Font Size Is: " + difference;
+    fill("blue");
+    text('Hello World', 200,200);
 }
 
 function gotPoses(results) {
     if(results.length>0) {
         console.log(results);
+        leftwristX = results[0].pose.leftWrist.x;
+        console.log(leftwristX);
+        rightwristX = results[0].pose.rightWrist.x;
+        console.log(rightwristX);
+        difference = floor(leftwristX-rightwristX);
     }
 }
+
